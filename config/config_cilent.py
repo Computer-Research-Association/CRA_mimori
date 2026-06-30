739f9c5703b6bc77595704bbb02751d102c1ae1f
HYC <yechanhwangahah@gmail.com>
2026-06-30 15:36:27 +0900

    :)

--- config/config_cilent.py
+++ config/config_cilent.py
@@ -19,5 +19,3 @@ TAVILY_SEARCH_DEPTH = "advanced"  # "basic" or "advanced"
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")
 YOUTUBE_MAX_RESULTS = 30    # 키워드당 검색할 영상 개수
 YOUTUBE_MAX_COMMENTS = 50  #영상당 가져올 댓글 개수
-TAVILY_MAX_RESULTS = 10
-TAVILY_SEARCH_DEPTH = "advanced"  # "basic" or "advanced"
