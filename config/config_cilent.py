f4067492912de39ff4cf54bfaef72e4f9dce1282
HYC <yechanhwangahah@gmail.com>
2026-07-06 15:54:12 +0900

    qdrant 클라우드 설정

--- config/config_cilent.py
+++ config/config_cilent.py
@@ -35,3 +35,4 @@ USER_AGENTS = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
 ]
+
