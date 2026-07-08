6f57fd7f18fd8afa9c24b1b6287c70932478dbd1
HYC <yechanhwangahah@gmail.com>
2026-07-08 15:27:55 +0900

    fix: parent_lookup이 memes 대신 cleaned_memes 조회하도록 수정

--- preprocessing/parent_lookup.py
+++ preprocessing/parent_lookup.py
@@ -6,14 +6,13 @@ Qdrant 검색 후 top-k 청크를 얻었을 때 LLM에 넘길 원본 문서가 �
 이 모듈로 한 번에 가져온다. $in 쿼리로 N번 왕복을 1번으로 줄임.
 """
 
-from bson import ObjectId
-
+from config.config_cilent import CLEANED_COLLECTION
 from DB.mongo_client import get_collection
 
 
 def get_parent(parent_id) -> dict | None:
     """단건 조회. 없으면 None."""
-    return get_collection().find_one({"_id": parent_id})
+    return get_collection(CLEANED_COLLECTION).find_one({"_id": parent_id})
 
 
 def get_parents(parent_ids) -> dict:
@@ -22,5 +21,5 @@ def get_parents(parent_ids) -> dict:
     Qdrant top-k 결과가 여러 문서에 걸칠 때 N번 쿼리를 방지.
     """
     ids = list(parent_ids)
-    docs = get_collection().find({"_id": {"$in": ids}})
+    docs = get_collection(CLEANED_COLLECTION).find({"_id": {"$in": ids}})
     return {doc["_id"]: doc for doc in docs}
